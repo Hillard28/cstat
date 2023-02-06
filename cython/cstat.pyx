999d0ffb1f16d286b9fe70912bd399d76143e924
Ryan Gilland <ryangilland@gmail.com>
2023-02-05 22:13:03 -0500

    Update cstat.pyx

--- cython/cstat.pyx
+++ cython/cstat.pyx
@@ -6,22 +6,22 @@ Created on Wed Feb  1 11:54:51 2023
 """
 
 cdef extern from "sgd.h":
-    double dsgd(unsigned long m, unsigned long n, double *X, double *y, double *coef, double *intercept, double eta, int max_iter, int fit_intercept, int random_seed)
+    double dsgd(size_t m, size_t n, const double *X, const double *y, double *coef, double *intercept, double eta, int max_iter, int fit_intercept, int random_seed)
 
 cdef extern from "gd.h":
-    double dgd(unsigned long m, unsigned long n, double *X, double *y, double *coef, double *intercept, double eta, int max_iter, int fit_intercept)
+    double dgd(size_t m, size_t n, const double *X, const double *y, double *coef, double *intercept, double eta, int max_iter, int fit_intercept)
 
 def sgd(m, n, X, y, coef, intercept, eta, max_iter, fit_intercept, random_seed):
-    cdef double[:] X_view = X
-    cdef double[:] y_view = y
+    cdef const double[:] X_view = X
+    cdef const double[:] y_view = y
     cdef double[:] coef_view = coef
     cdef double[:] intercept_view = intercept
     dsgd(m, n, &X_view[0], &y_view[0], &coef_view[0], &intercept_view[0], eta, max_iter, int(fit_intercept), random_seed)
     return coef, intercept
 
 def gd(m, n, X, y, coef, intercept, eta, max_iter, fit_intercept):
-    cdef double[:] X_view = X
-    cdef double[:] y_view = y
+    cdef const double[:] X_view = X
+    cdef const double[:] y_view = y
     cdef double[:] coef_view = coef
     cdef double[:] intercept_view = intercept
     dgd(m, n, &X_view[0], &y_view[0], &coef_view[0], &intercept_view[0], eta, max_iter, int(fit_intercept))
