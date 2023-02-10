37f90fd2e1f1aa1838d997bca492cf8881abbb92
Ryan Gilland <ryangilland@gmail.com>
2023-02-10 14:42:00 -0500

    Update setup.py

--- cython/setup.py
+++ cython/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-name="cstat"
-sources=["cstat.pyx", "gd.c", "sgd.c", "utils.c"]
-extensions = Extension(name, sources, extra_compile_args=["-O3"])
+name="logistic"
+sources=["logistic.pyx", "gd.c", "sgd.c", "utils.c"]
+extensions = Extension(name, sources, extra_compile_args=["-O3", "-march=native"])
 
 setup(ext_modules = cythonize(
      extensions,
