14de79507ddd1ab5d24bb09efde9f38aa930a9e1
Ryan Gilland <ryangilland@gmail.com>
2023-05-15 16:16:07 -0400

    Update gd.c
    Remove redundant <stdlib.h> dependency.

--- src/gd.c
+++ src/gd.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "utils.h"
 
 void dgd( size_t m, size_t n, const double *X, const double *y, double *coef, double *intercept, double eta, int max_iter, int fit_intercept )
