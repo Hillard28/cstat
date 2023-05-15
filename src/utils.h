fc06b951ec97047be4f3f3a08450160d20afc203
Ryan Gilland <ryangilland@gmail.com>
2023-05-15 16:17:28 -0400

    Update utils.h
    Add code for sorting and dropping duplicates.

--- src/utils.h
+++ src/utils.h
@@ -16,12 +16,22 @@ unsigned long genRandLong(MTRand* rand);
 
 double genRand(MTRand* rand);
 
-double dlc( size_t n, const double *X, const double *coef, double intercept );
+// Compute z = w * x + b
+double dlc( unsigned long n, double *X, double *coef, double intercept );
 
-double dlcx2( size_t n, const double *X, const double *coef, double intercept, int n_odd );
+// Compute y_hat = 1 / (1 + e^(-z))
+double dsigmoid( unsigned long n, double alpha, double *X, double *coef, double beta, double intercept );
 
-//double dsigmoid( size_t n, double alpha, const double *X, double *coef, double beta, double intercept );
+// Function to swap two elements in the array
+void swap(int* a, int* b);
 
-double dsigmoid( size_t n, const double *X, double *coef, double intercept, int n_odd );
+// Function to partition the array and return the pivot index
+int partition(double *feature_values, int low, int high);
+
+// Quicksort function
+void quicksort(double *feature_values, int low, int high);
+
+// Function to drop duplicates
+unsigned long drop_duplicates(double *feature_values, unsigned long m);
 
 #endif
