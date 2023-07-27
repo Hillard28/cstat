a008bed153ce328bc6a775a289b1259fb3d8ef7c
Ryan Gilland <ryangilland@gmail.com>
2023-07-27 13:41:41 -0400

    Add files via upload

--- src/utils.h
+++ src/utils.h
@@ -16,13 +16,11 @@ unsigned long genRandLong(MTRand* rand);
 
 double genRand(MTRand* rand);
 
-double dlc( size_t n, const double *X, const double *coef, double intercept );
+// Compute z = w * x + b
+double dlc( unsigned long n, double *X, double *coef, double intercept );
 
-double dlcx2( size_t n, const double *X, const double *coef, double intercept, int n_odd );
-
-//double dsigmoid( size_t n, double alpha, const double *X, double *coef, double beta, double intercept );
-
-double dsigmoid( size_t n, const double *X, double *coef, double intercept, int n_odd );
+// Compute y_hat = 1 / (1 + e^(-z))
+double dsigmoid( unsigned long n, double alpha, double *X, double *coef, double beta, double intercept );
 
 // Function to swap two elements in the array
 void swap(int* a, int* b);
